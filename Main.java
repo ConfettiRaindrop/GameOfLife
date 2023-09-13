4f8e892e5ad618bc462c4dea31ab1443cea733bb
Camila Figueira <60558085+ConfettiRaindrop@users.noreply.github.com>
2023-09-13 18:34:07 +0000

    start UI testing - swingx

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import javax.swing.JFrame;
 
 public class Main {
     /*A living cell with two or three living neighbors survives to the next time step.
@@ -5,7 +6,21 @@ public class Main {
         A dead cell with exactly three neighbors will be alive in the next time step. */
   public static void main(String[] args) {
     Board game = new Board();
-    game.runGame();
+    
+    javax.swing.SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        testUI();
+      }
+    });
+    //game.runGame();
+  }
+
+  public static void testUI() {
+    JFrame frame = new JFrame("worldOfLife");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    frame.pack();
+    frame.setVisible(true);
   }
 
 }
